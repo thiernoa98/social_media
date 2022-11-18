6d366bc5ae7a188555915bc9695aa2a505bec637
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2022-11-17 20:29:20 -0500

    Update friends.php

--- friends.php
+++ friends.php
@@ -1,29 +1,28 @@
 <div id="friends">
 
-		<!-- check if user is female or male-->
-	<?php 
-	//assign image to female first to avoid using else statement
-	$image = "Female.jpg";
-	if ($row['gender'] == "Male") 
-	{
-		$image = "Male.jpg";
-	}
+<?php 
+//assign image to female frst, avoid using else statement
+$image = "assets/Female.jpg";
+if ($row['gender'] == "Male") 
+{
+	$image = "assets/Male.jpg";
+}
 
-    if (file_exists($row['profile_img'])) 
-	{
-		//get_thumb_profile shape the image
-		$image = get_thumb_profile($row['profile_img']);
-	}
-
-	?>
-
-	<!-- we are using query string(?) to assign each user to its profile -->
-	<a href="profile.php?id=<?php echo $row['user_id'] ?>">
-
-	<img id="friend_img" src="<?php echo $image ?>">
+if (file_exists($row['profile_img'])) 
+{
 	
-	<?php echo $row['first_name'] . " " . $row['last_name']; ?>
+	$image = get_thumb_profile($row['profile_img']);
+}
 
-	</a>
+?>
+
+<!-- using query string(?) to assign each user to its profile -->
+<a href="profile.php?id=<?php echo $row['user_id'] ?>">
+
+<img id="friend_img" src="<?php echo $image ?>">
+
+<?php echo $row['first_name'] . " " . $row['last_name']; ?>
+
+</a>
 
 </div>
