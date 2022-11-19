15d26f64d1fd194d2f175cef34bac7f7e2b9b7c4
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2022-11-18 19:51:29 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,50 @@
+
+<?php 
+//$curent_user = $user_data;
+
+//assigning the corner image
+$corner_img = "Male.jpg";
+if (isset($curent_user) && file_exists($curent_user['profile_img'])) 
+{
+	//get_thumb_profile shape the image
+	$corner_img = get_thumb_profile($curent_user['profile_img']);
+}
+else
+{
+	if ($curent_user['gender'] == "Male") 
+	{
+		$corner_img = "Male.jpg"; 
+		
+	}
+	else
+	{
+		$corner_img = "Female.jpg";
+	}
+}
+
+
+?>
+
+
+<div id="grey_bar">
+	<form method="get" action="search.php">
+		<div style="width: 800px; margin: auto; font-size: 35px;">
+		
+			<a href="Timeline.php" style="text-decoration: none; ">
+				<?php echo "<img src='home.jpg' style='width:35px'"?> </br>
+				Rush 
+			</a> 
+
+			<!-- search bar-->
+			&nbsp &nbsp &nbsp <input type="text" id="search_box" name="find" placeholder="search for people">
+			
+
+			<a href="profile.php?id=<?php echo $_SESSION['user_id'] ?>"> 
+			<img src="<?php echo $corner_img ?>" style="width:40px; float: right;">
+			</a>
+			<a href="logout.php">
+				<span style="font-size: 15px; float: right; margin: 10px;"> Sign out</span>
+			</a>
+		</div>
+	</form>
+</div>
