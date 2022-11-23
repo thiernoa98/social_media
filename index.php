91bd280a631d71d4fd86111c4796908f54aa2711
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2022-11-22 23:41:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <!-- this page is empty because of security reason,
- this way hackers can't access our data through image-->
+ this way hackers can't access the data through the images-->
 <h1 style="color: red;"> Error 404</h1>
