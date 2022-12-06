405fc9894c62a9d95ad97f8813236b9f7a429ead
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2022-12-06 16:35:58 -0500

    Update profile_followers.php

--- profile_followers.php
+++ profile_followers.php
@@ -3,31 +3,22 @@
 	<?php 
 
 	$followers = get_likes($user_data['user_id'], "user",$con);
-
-
 		if ($followers) 
 		{
 
 			foreach ($followers as $row) 
 			{
-				//$row =... has full data of users, it connects it with the likes table
 				$row = get_user($row['user_id'],$con);
 				include("friends.php");
 
 			}
 			
 		}
-	
-	
 		else
 
 		{
 			echo "<h2 style='color:#800080' >Sorry! But ".$user_data['first_name']. " has no followers";	
 		}
-			
-		
-	
-
 
 	?>
 
