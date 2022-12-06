13bc96b7dac2ef3b8475a025ff6491ede4c8eb36
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2022-12-06 16:34:22 -0500

    Add files via upload

---
+++ profile_followers.php
@@ -0,0 +1,38 @@
+<!-- post area, images will be within this div-->
+<div style=" min-height:400px; width: 100%; background-color:lightgrey; text-align: center;" >
+	<div style="padding: 10%;">
+	<?php 
+
+	$followers = get_likes($user_data['user_id'], "user",$con);
+
+
+		if ($followers) 
+		{
+
+			foreach ($followers as $row) 
+			{
+				//$row =... has full data of users, it connects it with the likes table
+				$row = get_user($row['user_id'],$con);
+				include("friends.php");
+
+
+			}
+			
+		}
+	
+	
+		else
+
+		{
+			echo "<h2 style='color:#800080' >Sorry! But ".$user_data['first_name']. " has no followers";	
+		}
+			
+		
+	
+
+
+	?>
+
+
+	</div>
+</div>
