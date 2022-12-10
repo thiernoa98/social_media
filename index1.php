def1a46e3c7e9bbe1cf4d47929345d0ed17f8d19
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2022-12-09 19:13:09 -0500

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("function.php");
+
+	$user_data = check_login($con);  
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Rush</title>
+	<style type="text/css">
+	body{ background-image:url("nyc.jpg"); background-repeat:no-repeat; height:450px;background-size:cover;background-position:center;padding-top:30px; text-align:center; color: white;
+		position:relative;}
+		</style>
+</head>
+
+<body>
+
+<a href="profile.php"> profile</a>
+	<a href="logout.php"><p style="text-align: right;" >Logout</p></a>
+	<h1>This is the index page </h1>
+	<br>
+
+	Hello <?php echo $user_data['first_name']?>, WELCOME!
+
+
+</body>
+</html>
